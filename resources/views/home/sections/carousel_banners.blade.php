f8f89279818cd916167b50fb611de88046a7d995
SilverAstarot <silverastarot@gmail.com>
2021-05-19 12:21:56 -0400

    Update carousel_banners.blade.php

--- resources/views/home/sections/carousel_banners.blade.php
+++ resources/views/home/sections/carousel_banners.blade.php
@@ -13,7 +13,10 @@
         loop:true,
         margin:0,
         nav:true,
-        navText: ['<img src="{{'icons/arrow_left.svg'}}"/>', '<img src="{{'icons/arrow_right.svg'}}"/>'],
+        navText: [
+            '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path fill="#fff" d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>',
+            '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path fill="#fff" d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>'
+            ],
         responsive:{
             0:{
                 items:1
