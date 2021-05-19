10e8dbcea19853ef80e5d39b252895497ad3908d
SilverAstarot <silverastarot@gmail.com>
2021-05-19 18:22:19 -0400

    Update categories.blade.php

--- resources/views/vitrina/categories.blade.php
+++ resources/views/vitrina/categories.blade.php
@@ -9,27 +9,27 @@
     </div>
 
     <div class="flex justify-center mt-4">
-        <div class="max-w-7xl w-full shadow rounded-md p-6 md:px-10 bg-white">
+        <div class="grid grid-cols-1 md:grid-cols-2 max-w-7xl w-full shadow rounded-md p-6 md:px-10 bg-white">
             @foreach ($principal_categories as $category)
                 <div class="text-gray-900 font-bold text-lg my-4">
-                    <a class="hover:text-blue-600" href="{{route('products.category.show', $category->slug)}}">
+                    <a class="hover:text-blue-600 mb-3" href="{{route('products.category.show', $category->slug)}}">
                         {{ $category->category }}
                         <span class="text-gray-600">({{ $category->products->count() }})</span>
                     </a>
-                </div>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mb-4">
-                    @foreach ($categories_children as $category_child)
-                        @if ($category->id == $category_child->padre_id)
-                            <div class="col-span-1">
-                                <a class="text-gray-900 hover:text-blue-800" href="{{route('products.category.show', $category_child->slug)}}">
-                                    {{ $category_child->category }} ({{ $category_child->products->count() }})
-                                </a>
-                            </div>
-                        @endif
-                    @endforeach
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mb-4">
+                        @foreach ($categories_children as $category_child)
+                            @if ($category->id == $category_child->padre_id)
+                                <div class="col-span-1">
+                                    <a class="text-gray-900 hover:text-blue-800" href="{{route('products.category.show', $category_child->slug)}}">
+                                        {{ $category_child->category }} ({{ $category_child->products->count() }})
+                                    </a>
+                                </div>
+                            @endif
+                        @endforeach
 
+                    </div>
+                    <hr>
                 </div>
-                <hr>
             @endforeach
         </div>
     </div>
