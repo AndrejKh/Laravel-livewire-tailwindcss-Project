1d2c4b7fb75c895a52b79abd74cab1024150b64d
SilverAstarot <silverastarot@gmail.com>
2021-05-19 21:47:33 -0400

    Update brand-component.blade.php

--- resources/views/cms/tiendas/components/brand-component.blade.php
+++ resources/views/cms/tiendas/components/brand-component.blade.php
@@ -52,9 +52,9 @@
                         <span>Selecciona la imagen de la marca </span>
                         <label class="text-green-600 cursor-pointer mb-2" for="profile_photo_path_brand"> aqui</label>
                         <input type="file" class="hidden" id="profile_photo_path_brand" wire:model="profile_photo_path_brand">
-                        <span class="text-green-500" wire:loading wire:target="profile_photo_path_brand">
-                            Cargando...
-                        </span>
+                            <div class="text-center text-green-500" wire:loading wire:target="profile_photo_path_brand">
+                                Cargando...
+                            </div>
                         @if ($profile_photo_path_brand)
                             <div class="bg-no-repeat bg-cover bg-center w-20 h-20 overflow-hidden rounded-full mx-auto" style="background-image: url('{{ $profile_photo_path_brand->temporaryUrl() }}');"></div>
                         @endif
