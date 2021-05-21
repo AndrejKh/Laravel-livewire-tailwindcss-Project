21e743b775e89b953eafa09992cdabafb2aab264
SilverAstarot <silverastarot@gmail.com>
2021-05-20 23:19:45 -0400

    Update product_detail.blade.php

--- resources/views/vitrina/product_detail.blade.php
+++ resources/views/vitrina/product_detail.blade.php
@@ -38,7 +38,7 @@
                         </div>
                     </div>
 
-                    <div class="flex justify-center md:justify-start my-6">
+                    <div class="flex justify-center md:justify-start my-2 md:my-6">
                         <div class="flex bg-white rounded-full">
                             <span class="flex-shrink-0 self-center bg-green-400 rounded-full p-2 cursor-pointer" id="subtractProduct">
                                 <svg class="fill-current text-white h-7 w-7 lg:h-10 lg:w-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 5v11.17l-4.88-4.88c-.39-.39-1.03-.39-1.42 0-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0l6.59-6.59c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L13 16.17V5c0-.55-.45-1-1-1s-1 .45-1 1z"/></svg>
