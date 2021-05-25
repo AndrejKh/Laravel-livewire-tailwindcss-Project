d28c4f24e33095a612f6b37356b62d99e6c46629
SilverAstarot <silverastarot@gmail.com>
2021-05-25 16:52:43 -0400

    Tag Manager Google Analitics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WQG59HF');</script>
+        <!-- End Google Tag Manager -->
+
         <title>{{ config('app.name', 'Kabasto') }}</title>
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.png') }}">
 
@@ -27,6 +35,11 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased bg-gray-100 overflow-y-auto">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQG59HF"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100 relative">
