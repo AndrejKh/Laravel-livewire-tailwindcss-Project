2b41efc6a41519bc6896b8b808c1f5372236c059
SilverAstarot <silverastarot@gmail.com>
2021-05-25 12:10:15 -0400

    address

--- resources/views/supermercados/card_tienda.blade.php
+++ resources/views/supermercados/card_tienda.blade.php
@@ -23,7 +23,14 @@
         <div>
             <span class="flex self-center text-sm text-gray-600">
                 <svg class="inline" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" width="18" fill="#999"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
-                <span class=" self-center"> {{ $tienda->user->state->state }}</span>
+                <span class="self-center">
+                    @isset( $tienda->address->state->state )
+                        {{ $tienda->address->state->state }}
+                    @endisset
+                    @isset( $tienda->address->city->city )
+                        , {{ $tienda->address->city->city }}
+                    @endisset
+                </span>
             </span>
         </div>
         <div>
--- resources/views/tienda/card_tienda.blade.php
+++ resources/views/tienda/card_tienda.blade.php
@@ -25,8 +25,12 @@
             <span class="flex self-center text-xs md:text-sm text-gray-600">
                 <svg class="inline h-4 md:h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
                 <span class="inline font-semibold">
-                    {{ $tienda->address->state->state }},
-                    {{ $tienda->address->city->city }},
+                    @isset( $tienda->address->state->state )
+                        {{ $tienda->address->state->state }}
+                    @endisset
+                    @isset( $tienda->address->city->city )
+                        , {{ $tienda->address->city->city }}
+                    @endisset
                     <span class="hidden md:inline"> {{ $tienda->address->address }} </span>
                 </span>
             </span>
