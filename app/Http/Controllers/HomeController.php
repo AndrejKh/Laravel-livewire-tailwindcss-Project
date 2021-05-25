1b90ffa6b9775b7293703c233d69564bf6b364cd
SilverAstarot <silverastarot@gmail.com>
2021-05-25 11:06:23 -0400

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,18 +30,12 @@ class HomeController extends Controller
         $carousel_banners = BannerPromocional::latest('id')->where('page', 'home')->where('status', 'active')->get();
         $banners_promotionals = BannerPromocional::latest('id')->where('page', 'promotions')->where('status', 'active')->get();
         $principal_categories = Category::where('status', 'active')->where('padre_id', 0)->take(7)->get();
-        $products = Product::latest('id')->where('status', 'active')->get();
-
-        $carousel_items_first = Product::latest('id')->where('status', 'active')->where('category_id', '3')->get();
-        $carousel_items_second = Product::latest('id')->where('status', 'active')->where('category_id', '2')->get();
-        $carousel_items_third = Product::latest('id')->where('status', 'active')->where('category_id', '7')->get();
-        $carousel_items_fourth = Product::latest('id')->where('status', 'active')->where('category_id', '4')->get();
-        $carousel_items_five = Product::latest('id')->where('status', 'active')->where('category_id', '5')->get();
+        $products = Product::inRandomOrder()->where('status', 'active')->get();
         $brands = Brand::latest('id')->where('status', 'active')->get();
 
         $categories = Category::inRandomOrder()->where('status', 'active')->where('padre_id', 0)->take(5)->get();
 
-        return view('home.home', compact('carousel_banners', 'categories', 'principal_categories','products','brands', 'banners_promotionals', 'carousel_items_first', 'carousel_items_second', 'carousel_items_third', 'carousel_items_fourth', 'carousel_items_five'));
+        return view('home.home', compact('carousel_banners', 'categories', 'principal_categories','products','brands', 'banners_promotionals'));
     }
 
     // Vitirna de productos, controlador del input search del navbar
