fd72295881a1dea35232906b1326b401214dc7ea
SilverAstarot <silverastarot@gmail.com>
2021-05-24 20:32:46 -0400

    Update product_detail.blade.php

--- resources/views/vitrina/product_detail.blade.php
+++ resources/views/vitrina/product_detail.blade.php
@@ -48,9 +48,6 @@
                                 <svg class="fill-current text-white h-7 w-7 lg:h-10 lg:w-10" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M13 19V7.83l4.88 4.88c.39.39 1.03.39 1.42 0 .39-.39.39-1.02 0-1.41l-6.59-6.59c-.39-.39-1.02-.39-1.41 0l-6.6 6.58c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L11 7.83V19c0 .55.45 1 1 1s1-.45 1-1z"/></svg>
                             </span>
                         </div>
-                        <div class="flex my-4">
-                            <a class="bg-green-500 text-white py-2 px-8 rounded-md shadow-md" href="{{ url()->previous() }}">Seguir comprando</a>
-                        </div>
                     </div>
                     {{-- Boton volver - seguir comprando --}}
                     <div class="flex my-4">
