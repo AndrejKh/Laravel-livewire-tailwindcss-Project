259683385738b07ac09b4415c452bd104893a35a
SilverAstarot <silverastarot@gmail.com>
2021-06-01 18:40:44 -0400

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -25,7 +25,6 @@ class OrderController extends Controller
 
     // Crear orden
     public function createOrder(Request $request){
-        return $request;
         if ( isset(Auth::user()->id) ) {
             $user_id = Auth::user()->id;
         }else{
@@ -47,32 +46,33 @@ class OrderController extends Controller
 
         $amountSelected = $request->amount;
         $amount = $amountSelected['amount'];
+        return $amount.'-'.$user_id.'-'.$brand_id;
 
-        $items = $request->items;
+        // $items = $request->items;
 
         // Creo la orden
-        Order::create([
-            'brand_id' => $brand_id,
-            'user_id' => $user_id,
-            'amount' => $amount
-        ]);
-        $newOrder = Order::latest('id')->first();
-        $order_id = $newOrder->id;
+        // Order::create([
+        //     'brand_id' => $brand_id,
+        //     'user_id' => $user_id,
+        //     'amount' => $amount
+        // ]);
+        // $newOrder = Order::latest('id')->first();
+        // $order_id = $newOrder->id;
 
-        foreach ($items as $item) {
-            $product_id = $item['product_id'];
-            $quantity = $item['quantity'];
-            $price = $item['price'];
-            # code...
-            OrderProducts::create([
-                'order_id' => $order_id,
-                'product_id' => $product_id,
-                'quantity' => $quantity,
-                'price' => $price
-            ]);
-        }
+        // foreach ($items as $item) {
+        //     $product_id = $item['product_id'];
+        //     $quantity = $item['quantity'];
+        //     $price = $item['price'];
+        //     # code...
+        //     OrderProducts::create([
+        //         'order_id' => $order_id,
+        //         'product_id' => $product_id,
+        //         'quantity' => $quantity,
+        //         'price' => $price
+        //     ]);
+        // }
 
-        return true;
+        // return true;
 
     }
 
