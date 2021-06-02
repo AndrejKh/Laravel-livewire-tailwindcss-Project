951d42889561b4e503748f7d7b3f2f9277602272
SilverAstarot <silverastarot@gmail.com>
2021-06-02 11:43:20 -0400

    Update 403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,24 +1,6 @@
-{{-- @extends('layouts.app')
-
-@section('title')
-    Kabasto.com - Página no accesible :\
-@endsection
-
-@section('content')
-
-    <div class="text-2xl font-bold text-gray-900 text-center mt-14">
-        Esta página no esta permitida para tí. <br> Debes iniciar sesión para acceder.
-    </div>
-    <div class="mt-10 text-center">
-        <img class="mx-auto mb-10 h-60" src="{{ asset('403.svg') }}" alt="imagen de página no permitida">
-        <a class="text-xl text-white bg-green-500 py-2 px-16 rounded-full shadow-md" href="{{ route('home') }}">Iniciar sesión</a>
-    </div>
-    <a class="text-lg text-gray-600 mt-4 block text-center mb-10" href="{{ route('register') }}">Registrarse</a>
-
-
-
-@endsection --}}
-
-<script>
+{{
+    redirect('/');
+}}
+{{-- <script>
     window.location.href = "/";
-</script>
+</script> --}}
