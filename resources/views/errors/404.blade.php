5f2c33650de1b62e8d5dd2a1dfbfe32ee5b5104b
SilverAstarot <silverastarot@gmail.com>
2021-06-02 11:18:55 -0400

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,12 +6,12 @@
 
 @section('content')
 
-    <div class="text-2xl font-bold text-gray-900 text-center mt-6">
+    <div class="text-2xl font-bold text-gray-900 text-center mt-14">
         No podemos encontrar la página que estas buscando
     </div>
     <div class="mt-10 text-center">
         <img class="mx-auto mb-10" src="{{ asset('404.svg') }}" alt="imagen de página no encontrada">
-        <a class="text-white bg-green-500 py-2 px-8 rounded-full shadow-md" href="{{ route('home') }}">Ir al inicio</a>
+        <a class="text-xl text-white bg-green-500 py-2 px-16 rounded-full shadow-md" href="{{ route('home') }}">Ir al inicio</a>
     </div>
 
 @endsection
