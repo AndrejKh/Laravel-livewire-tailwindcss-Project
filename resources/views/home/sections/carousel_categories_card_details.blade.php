8c96883182db61af22cda10ff7b94685ed18624d
SilverAstarot <silverastarot@gmail.com>
2021-06-07 08:18:12 -0400

    Update carousel_categories_card_details.blade.php

--- resources/views/home/sections/carousel_categories_card_details.blade.php
+++ resources/views/home/sections/carousel_categories_card_details.blade.php
@@ -2,9 +2,9 @@
 <div class="flex justify-center mt-3 px-2">
     <div class="max-w-7xl w-full">
         <h3 class="text-lg text-gray-800 px-2 inline">
-            Algunas categorías para tí
+            Algunas categorías para tí -
         </h3>
-        - <a class="inline text-blue-700 text-sm" href="{{ route('categorias') }}">Ver todas</a>
+        <a class="inline text-blue-700 text-sm" href="{{ route('categorias') }}">Ver todas</a>
         <p class="text-sm">Encuentra los diversos productos que estas buscando</p>
     </div>
 </div>
