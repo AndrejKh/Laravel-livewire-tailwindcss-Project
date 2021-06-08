8fff8e9902aa2cdf1f984ccf816dd06265c6b767
SilverAstarot <silverastarot@gmail.com>
2021-06-08 16:49:15 -0400

    Update card_category_details.blade.php

--- resources/views/components/card_category_details.blade.php
+++ resources/views/components/card_category_details.blade.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-1 lg:gap-2 rounded-md overflow-hidden shadow-md my-2 bg-white w-full">
-    <div class="col-span-1 lg:col-span-1 px-3 md:px-5 py-2 lg:py-3 text-left">
+    <div class="col-span-1 lg:col-span-1 px-3 md:px-5 py-2 lg:py-3 text-left self-center">
         <a class="text-gray-900 hover:text-gray-700 text-left font-bold text-lg" href="{{route('products.category.show', $category->slug)}}">
             {{ $category->category }}
         </a>
