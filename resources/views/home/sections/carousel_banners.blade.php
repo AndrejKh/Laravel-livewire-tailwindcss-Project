7f57c6c3bb23ee1dad1ac5d65c26dc6a2b8e271d
SilverAstarot <silverastarot@gmail.com>
2021-06-08 15:48:50 -0400

    Update carousel_banners.blade.php

--- resources/views/home/sections/carousel_banners.blade.php
+++ resources/views/home/sections/carousel_banners.blade.php
@@ -2,9 +2,9 @@
 <div class="owl-carousel owl-theme w-full" id="carousel_banners_home">
 
     @foreach ($carousel_banners as $banner)
-        <a class="item" href="{{ $banner->url }}">
-            <div class="bg-no-repeat bg-cover bg-center w-full h-36 md:h-60 lg:h-72 xl:h-72 overflow-hidden z-10 rounded-md shadow-md" style="background-image: url('/storage/{{ $banner->banner }}');"></div>
-        </a>
+        <div class="item h-36 md:h-60 lg:h-72 xl:h-80 ">
+            <img src="/storage/{{$banner->banner}}">
+        </div>
     @endforeach
 </div>
 <script>
