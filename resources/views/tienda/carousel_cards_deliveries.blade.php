4aaf0bbf05fde49c2cb52f431e99bccaa13de422
SilverAstarot <silverastarot@gmail.com>
2021-06-08 16:21:26 -0400

    Update carousel_cards_deliveries.blade.php

--- resources/views/tienda/carousel_cards_deliveries.blade.php
+++ resources/views/tienda/carousel_cards_deliveries.blade.php
@@ -25,12 +25,10 @@
                 <span class="text-xs sm:text-sm text-light text-white bg-green-450 px-3 py-1 rounded-full">
                     Delivery gratis
                 </span>
-                <span class="text-md text-gray-900 text-semibold block mt-1">
-                    A partir de
+                <span class="font-bold">
+                    En {{ $delivery->delivery_time }} Hrs.
                 </span>
-                <strong class="text-xl font-semibold text-gray-900 block">
-                    {{ $delivery->min_amount_purchase }} $
-                </strong>
+
                 @else
                 <span class="text-sm text-light text-white bg-blue-450 px-3 py-1 rounded-full">
                     {{ $delivery->min_amount_purchase }}$ Delivery
@@ -76,14 +74,17 @@
                         @endphp
                     </div>
                     <span class="col-span-1 text-right self-center">
-                        <div class="text-md text-gray-900 text-semibold block mt-1">
-                            Delivery en
-                        </div>
+                        <span class="text-md text-gray-900 text-semibold block mt-1">
+                            A partir de
+                        </span>
+                        <strong class="text-xl font-semibold text-gray-900 block">
+                            {{ $delivery->min_amount_purchase }} $
+                        </strong>
                         {{-- <svg class="h-5 inline" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.99 0C4.47 0 0 4.48 0 10C0 15.52 4.47 20 9.99 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 9.99 0ZM10 18C5.58 18 2 14.42 2 10C2 5.58 5.58 2 10 2C14.42 2 18 5.58 18 10C18 14.42 14.42 18 10 18Z" fill="black"/>
                             <path d="M10.5 5H9V11L14.25 14.15L15 12.92L10.5 10.25V5Z" fill="black"/>
                         </svg> --}}
-                        <span class="font-bold">{{ $delivery->delivery_time }} Hrs.</span>
+
                     </span>
                 </div>
             </div>
