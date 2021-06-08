243f75f78162477911cbd5ca78bfa7a8b624d86d
SilverAstarot <silverastarot@gmail.com>
2021-06-08 16:18:36 -0400

    Update carousel_cards_deliveries.blade.php

--- resources/views/tienda/carousel_cards_deliveries.blade.php
+++ resources/views/tienda/carousel_cards_deliveries.blade.php
@@ -75,7 +75,7 @@
                             echo $days;
                         @endphp
                     </div>
-                    <span class="col-span-1 text-right self-center font-bold">
+                    <span class="col-span-1 text-right self-center">
                         <div class="text-md text-gray-900 text-semibold block mt-1">
                             Delivery en
                         </div>
@@ -83,7 +83,7 @@
                             <path d="M9.99 0C4.47 0 0 4.48 0 10C0 15.52 4.47 20 9.99 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 9.99 0ZM10 18C5.58 18 2 14.42 2 10C2 5.58 5.58 2 10 2C14.42 2 18 5.58 18 10C18 14.42 14.42 18 10 18Z" fill="black"/>
                             <path d="M10.5 5H9V11L14.25 14.15L15 12.92L10.5 10.25V5Z" fill="black"/>
                         </svg> --}}
-                        <span>{{ $delivery->delivery_time }} Hrs.</span>
+                        <span class="font-bold">{{ $delivery->delivery_time }} Hrs.</span>
                     </span>
                 </div>
             </div>
