74e904b06520904d0eb32d80725a1eea09a3e4fa
SilverAstarot <silverastarot@gmail.com>
2021-06-10 20:16:58 -0400

    Update product_detail.blade.php

--- resources/views/vitrina/product_detail.blade.php
+++ resources/views/vitrina/product_detail.blade.php
@@ -11,7 +11,7 @@
     <!-- Primary Meta Tags -->
     <meta name="title" content="{{ ucwords($product->title) }} - Kabasto">
     <meta name="description" content="Precios de {{ $product->title }} en los abastos y supermercados de tu ciudad - Kabasto.com">
-    <meta name="keywords" content="precio de {{ $product->category->category }} en venezuela">
+    <meta name="keywords" content="precio de {{ $product->title }}">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
