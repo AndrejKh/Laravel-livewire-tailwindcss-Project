b3f05fc9fe1e83a8fcbb7194ccade5cb22b95377
SilverAstarot <silverastarot@gmail.com>
2021-06-10 21:01:22 -0400

    Update product_detail.blade.php

--- resources/views/tienda/product_detail.blade.php
+++ resources/views/tienda/product_detail.blade.php
@@ -11,7 +11,7 @@
     <!-- Primary Meta Tags -->
     <meta name="title" content="{{ ucwords($product->title) }} - Kabasto">
     <meta name="description" content="Los precios de {{ $product->title }} en {{$brand->brand}} en {{$brand->address->city->city}}">
-    <meta name="keywords" content="{{ $title }}">
+    <meta name="keywords" content="{{ $product->title }}">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
