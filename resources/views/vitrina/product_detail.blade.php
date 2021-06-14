8b61b4ccb2837bdcdd0434216443996307a8420e
SilverAstarot <silverastarot@gmail.com>
2021-06-14 15:04:37 -0400

    Update product_detail.blade.php

--- resources/views/vitrina/product_detail.blade.php
+++ resources/views/vitrina/product_detail.blade.php
@@ -59,9 +59,9 @@
                             Producto Disponible
                         </span>
                         @endif
-                        <h3 class="text-left text-2xl text-gray-900 font-semibold mt-4">
+                        <h2 class="text-left text-2xl text-gray-900 font-semibold mt-4">
                             {{ $product->title }}
-                        </h3>
+                        </h2>
                         <div class="text-left">
                             <a class="text-md text-gray-900" href="{{route('products.category.show', $product->category->slug)}}">
                                 Categoría: <span class="text-gray-400">{{ $product->category->category }}</span>
