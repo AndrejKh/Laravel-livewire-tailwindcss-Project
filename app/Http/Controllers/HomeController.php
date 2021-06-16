f50d4c28da3fa1249cab979711da4d92c14bd655
SilverAstarot <silverastarot@gmail.com>
2021-06-16 10:39:43 -0400

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
         $carousel_banners = BannerPromocional::latest('id')->where('page', 'home')->where('status', 'active')->get();
         $banners_promotionals = BannerPromocional::latest('id')->where('page', 'promotions')->where('status', 'active')->get();
         $principal_categories = Category::where('status', 'active')->where('padre_id', 0)->take(6)->get();
-        $products = Product::inRandomOrder()->where('status', 'active')->get();
+        $products = Product::inRandomOrder()->where('status', 'active')->take(15)->get();
 
         return view('home.home', compact('carousel_banners', 'principal_categories','products', 'banners_promotionals'));
     }
