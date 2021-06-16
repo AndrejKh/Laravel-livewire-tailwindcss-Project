577eb3bdfec1a2193af733c3d16ba9a4e01b9670
SilverAstarot <silverastarot@gmail.com>
2021-06-15 20:46:41 -0400

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -114,7 +114,7 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center md:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-900 focus:outline-none focus:text-gray-800 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-900 focus:outline-none focus:text-gray-800 transition duration-150 ease-in-out" aria-label="desplegar menu para celulares">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
