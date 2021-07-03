24130b4909502de45bb84100b99d7e6e3d386b05
SilverAstarot <silverastarot@gmail.com>
2021-07-02 22:56:57 -0400

    Update buttom_float_shopping_car.blade.php

--- resources/views/common/buttom_float_shopping_car.blade.php
+++ resources/views/common/buttom_float_shopping_car.blade.php
@@ -2,12 +2,12 @@
 
     <div class="fixed mb-1 bottom-5 right-3 mr-2 z-30" id="compareFloatButton">
         <a class="flex h-11 w-11 self-center text-center bg-blue-600 rounded-full shadow-md items-center" href="{{ route('comparar') }}" aria-label="ir a comparar precios">
-            <img class="mx-auto" src="{{ asset('compare.svg') }}" alt="icono de comparar precios">
+            <img class="mx-auto" src="{{ asset('compare.svg') }}" alt="icono de comparar precios" width="24px" height="24px">
         </a>
     </div>
 
     <div class="flex fixed bottom-5 right-4 h-14 w-14 self-center text-center bg-green-500 rounded-full z-30 shadow-md items-center cursor-pointer shoppingCarButtonOpenModal">
-        <img class="mx-auto" src="{{ asset('shopping_car_white.svg') }}" alt="icono de carrito de compras">
+        <img class="mx-auto" src="{{ asset('shopping_car_white.svg') }}" alt="icono de carrito de compras" width="30px" height="25px">
         <span class="absolute -top-1 -right-2 bg-blue-700 w-7 h-7 rounded-full justify-center items-center hidden" id="spanQuantityFloatButtonShoppingCard">
             <span class="text-xs text-white"></span>
         </span>
