bdab983a30fbe59231c0a0e9698b885837402cdc
SilverAstarot <silverastarot@gmail.com>
2021-07-02 22:36:21 -0400

    miscelaneos

--- resources/views/common/buttom_float_shopping_car.blade.php
+++ resources/views/common/buttom_float_shopping_car.blade.php
@@ -2,14 +2,12 @@
 
     <div class="fixed mb-1 bottom-5 right-3 mr-2 z-30" id="compareFloatButton">
         <a class="flex h-11 w-11 self-center text-center bg-blue-600 rounded-full shadow-md items-center" href="{{ route('comparar') }}" aria-label="ir a comparar precios">
-            <svg class="mx-auto fill-current text-white" xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 0 24 24" width="28px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9.01 14H3c-.55 0-1 .45-1 1s.45 1 1 1h6.01v1.79c0 .45.54.67.85.35l2.78-2.79c.19-.2.19-.51 0-.71l-2.78-2.79c-.31-.32-.85-.09-.85.35V14zm5.98-2.21V10H21c.55 0 1-.45 1-1s-.45-1-1-1h-6.01V6.21c0-.45-.54-.67-.85-.35l-2.78 2.79c-.19.2-.19.51 0 .71l2.78 2.79c.31.31.85.09.85-.36z"/></svg>
+            <img class="mx-auto" src="{{ asset('compare.svg') }}" alt="icono de comparar precios">
         </a>
     </div>
 
     <div class="flex fixed bottom-5 right-4 h-14 w-14 self-center text-center bg-green-500 rounded-full z-30 shadow-md items-center cursor-pointer shoppingCarButtonOpenModal">
-        <svg class="mx-auto" width="30" height="25" viewBox="0 0 202 185" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M146.686 110C154.183 110 160.78 105.9 164.179 99.7L199.966 34.8C203.664 28.2 198.866 20 191.269 20L43.324 20L33.9275 0L0 8.39138L21.2322 20L57.2188 95.9L43.7239 120.3C36.4266 133.7 46.023 150 61.2174 150L180.468 139.49L181.229 139.55L61.2174 130L72.2133 110H146.686ZM52.8205 40L174.275 40L146.686 90H76.5117L52.8205 40ZM61.2174 160C50.2214 160 41.3247 169 41.3247 180C41.3247 191 50.2214 180 61.2174 180C72.2133 180 81.2099 191 81.2099 180C81.2099 169 72.2133 160 61.2174 160ZM161.18 160C150.184 160 141.288 169 141.288 180C141.288 191 150.184 180 161.18 180C172.176 180 181.173 191 181.173 180C181.173 169 172.176 160 161.18 160Z" fill="white"/>
-        </svg>
+        <img class="mx-auto" src="{{ asset('shopping_car_white.svg') }}" alt="icono de carrito de compras">
         <span class="absolute -top-1 -right-2 bg-blue-700 w-7 h-7 rounded-full justify-center items-center hidden" id="spanQuantityFloatButtonShoppingCard">
             <span class="text-xs text-white"></span>
         </span>
--- resources/views/common/shopping_modal_car.blade.php
+++ resources/views/common/shopping_modal_car.blade.php
@@ -13,9 +13,7 @@
         <div class="grid grid-cols-5 px-3 py-5">
             <h5 class="col-span-4 text-xl font-bold text-gray-900">Carrito de compras</h5>
             <span class="justify-self-end">
-                <svg class="cursor-pointer" width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg" id="shoppingCarButtonModalClose">
-                    <path d="M12.639 11.0735L23.3484 0.993304C23.5898 0.766088 23.5898 0.3977 23.3484 0.170484C23.1069 -0.0567312 22.7156 -0.0567312 22.4741 0.170484L11.7648 10.2506L1.05536 0.170412C0.813966 -0.0568039 0.422582 -0.0568039 0.181184 0.170412C-0.0602917 0.397627 -0.0602917 0.766016 0.181184 0.993231L10.8905 11.0735L0.181107 21.1537C-0.0603689 21.3809 -0.0603689 21.7493 0.181107 21.9766C0.301806 22.0902 0.459982 22.147 0.618236 22.147C0.776489 22.147 0.934665 22.0902 1.05536 21.9766L11.7648 11.8964L22.4741 21.9766C22.5949 22.0902 22.753 22.147 22.9113 22.147C23.0695 22.147 23.2277 22.0902 23.3484 21.9766C23.5898 21.7493 23.5898 21.3809 23.3484 21.1537L12.639 11.0735Z" fill="black"/>
-                </svg>
+                <img class="h-6 cursor-pointer" src="{{ asset('close_icon_dark.svg') }}" alt="icono para cerrar modal de carrito de compras">
             </span>
         </div>
 
@@ -38,9 +36,7 @@
                 <div hidden id="cardBrandShoppingCar">
                     <article class="grid grid-cols-5 md:grid-cols-5 lg:grid-cols-6 p-2 md:p-3 lg:p-4 shadow-md bg-white rounded-lg relative">
                         <span class="w-6 h-6 md:w-7 md:h-7 absolute -top-2 -right-2 rounded-full bg-gray-600 text-white flex items-center justify-center cursor-pointer" id="deleteBrandDetailInShoppingCar">
-                            <svg class="h-2 cursor-pointer stroke-current stroke-3" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12.639 11.0735L23.3484 0.993304C23.5898 0.766088 23.5898 0.3977 23.3484 0.170484C23.1069 -0.0567312 22.7156 -0.0567312 22.4741 0.170484L11.7648 10.2506L1.05536 0.170412C0.813966 -0.0568039 0.422582 -0.0568039 0.181184 0.170412C-0.0602917 0.397627 -0.0602917 0.766016 0.181184 0.993231L10.8905 11.0735L0.181107 21.1537C-0.0603689 21.3809 -0.0603689 21.7493 0.181107 21.9766C0.301806 22.0902 0.459982 22.147 0.618236 22.147C0.776489 22.147 0.934665 22.0902 1.05536 21.9766L11.7648 11.8964L22.4741 21.9766C22.5949 22.0902 22.753 22.147 22.9113 22.147C23.0695 22.147 23.2277 22.0902 23.3484 21.9766C23.5898 21.7493 23.5898 21.3809 23.3484 21.1537L12.639 11.0735Z" fill="white"/>
-                            </svg>
+                            <img class="h-2 cursor-pointer" src="{{ asset('close_icon.svg') }}" alt="icono para cerrar modal de carrito de compras">
                         </span>
                         <div class="col-span-1 self-center">
                             <div class="bg-no-repeat bg-cover bg-center h-12 w-12 md:w-16 md:h-16 overflow-hidden rounded-full mx-auto" id="imgCardBrandShoppingCar"></div>
@@ -48,7 +44,7 @@
                         <div class="col-span-4 lg:col-span-5">
                             <a class="font-semibold text-lg inline" id="titleCardBrandShoppingCar" href="#"></a>
                             <div class="flex self-center text-xs md:text-sm text-gray-600">
-                                <svg class="inline h-4 md:h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
+                                <img class="inline h-4 md:h-5" src="{{ asset('address.svg') }}" alt="icono de la ubicacion del abasto o supermercado">
                                 <h2 class="inline font-semibold" id="addressCardBrandShoppingCar"></h2>
                             </div>
                         </div>
@@ -58,10 +54,8 @@
                 {{-- Card de Ejemplo para los productos--- NO Eleiminar FUNDAMENTAL para el Carrito de compras --}}
                 <div hidden id="cardProductShoppingCar">
                     <div class="max-w-7xl w-full rounded-xl shadow-md relative bg-white mb-2">
-                        <span class="w-6 h-6 absolute -top-2 -right-2 rounded-full bg-gray-600 text-white flex items-center justify-center cursor-pointer removeProductModalShoppingCar">
-                            <svg class="h-2 md:h-2 cursor-pointer stroke-current stroke-3" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg" id="">
-                                <path d="M12.639 11.0735L23.3484 0.993304C23.5898 0.766088 23.5898 0.3977 23.3484 0.170484C23.1069 -0.0567312 22.7156 -0.0567312 22.4741 0.170484L11.7648 10.2506L1.05536 0.170412C0.813966 -0.0568039 0.422582 -0.0568039 0.181184 0.170412C-0.0602917 0.397627 -0.0602917 0.766016 0.181184 0.993231L10.8905 11.0735L0.181107 21.1537C-0.0603689 21.3809 -0.0603689 21.7493 0.181107 21.9766C0.301806 22.0902 0.459982 22.147 0.618236 22.147C0.776489 22.147 0.934665 22.0902 1.05536 21.9766L11.7648 11.8964L22.4741 21.9766C22.5949 22.0902 22.753 22.147 22.9113 22.147C23.0695 22.147 23.2277 22.0902 23.3484 21.9766C23.5898 21.7493 23.5898 21.3809 23.3484 21.1537L12.639 11.0735Z" fill="white"/>
-                            </svg>
+                        <span class="w-7 h-7 absolute -top-2 -right-2 rounded-full bg-gray-600 text-white flex items-center justify-center cursor-pointer removeProductModalShoppingCar">
+                            <img class="h-3 cursor-pointer" src="{{ asset('close_icon.svg') }}" alt="icono para cerrar modal de carrito de compras">
                         </span>
                         <div class="grid grid-cols-3 px-2">
                             <div class="col-span-1">
@@ -123,19 +117,19 @@
             <div class="bg-gray-100 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <span class="mr-0" id="finalizarCompraButton" style="display:none;">
                     <div class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:blue-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5 items-center cursor-pointer" aria-label="ir a comparar precios" id="finalizarCompra">
-                        <svg class="fill-current text-white mr-2" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><rect fill="none" height="24" width="24"/><path d="M14.29,5.71L14.29,5.71c-0.39,0.39-0.39,1.02,0,1.41L18.17,11H3c-0.55,0-1,0.45-1,1v0c0,0.55,0.45,1,1,1h15.18l-3.88,3.88 c-0.39,0.39-0.39,1.02,0,1.41l0,0c0.39,0.39,1.02,0.39,1.41,0l5.59-5.59c0.39-0.39,0.39-1.02,0-1.41L15.7,5.71 C15.32,5.32,14.68,5.32,14.29,5.71z"/></svg>
+                        <img class="mr-2" src="{{ asset('arrow_finally.svg') }}" alt="icono de flecha para finalizar compra en kabasto">
                         Finalizar compra
                     </div>
                 </span>
                 <span class="hidden lg:mr-2 mt-3 md:mt-0" id="compararButton">
                     <a class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-blue-600 hover:bg-blue-500 text-base leading-6 font-medium text-white shadow-sm focus:outline-none focus:blue-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5 items-center" href="{{ route('comparar') }}" aria-label="ir a comparar precios">
-                        <svg class="fill-current text-white mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6.14 11.86l-2.78 2.79c-.19.2-.19.51 0 .71l2.78 2.79c.31.32.85.09.85-.35V16H13c.55 0 1-.45 1-1s-.45-1-1-1H6.99v-1.79c0-.45-.54-.67-.85-.35zm14.51-3.21l-2.78-2.79c-.31-.32-.85-.09-.85.35V8H11c-.55 0-1 .45-1 1s.45 1 1 1h6.01v1.79c0 .45.54.67.85.35l2.78-2.79c.2-.19.2-.51.01-.7z"/></svg>
+                        <img src="{{ asset('compare.svg') }}" alt="icono de comparar precios de supermercados y abastos">
                         Comparar precios
                     </a>
                 </span>
                 <span class="hidden lg:mr-2 mt-3 md:mt-0" id="compararButtonOutline">
                     <a class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-white hover:bg-gray-200 text-base leading-6 font-medium text-blue-600 shadow-sm focus:outline-none focus:blue-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5 items-center" href="{{ route('comparar') }}" aria-label="ir a comparar precios">
-                        <svg class="fill-current text-blue-600 mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6.14 11.86l-2.78 2.79c-.19.2-.19.51 0 .71l2.78 2.79c.31.32.85.09.85-.35V16H13c.55 0 1-.45 1-1s-.45-1-1-1H6.99v-1.79c0-.45-.54-.67-.85-.35zm14.51-3.21l-2.78-2.79c-.31-.32-.85-.09-.85.35V8H11c-.55 0-1 .45-1 1s.45 1 1 1h6.01v1.79c0 .45.54.67.85.35l2.78-2.79c.2-.19.2-.51.01-.7z"/></svg>
+                        <img class="mr-2" src="{{ asset('compare_blue.svg') }}" alt="icono de comparar precios de supermercados y abastos">
                         Comparar precios
                     </a>
                 </span>
