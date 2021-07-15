083e251c4304214e2f149d3795cf36595b18bf5b
SilverAstarot <silverastarot@gmail.com>
2021-07-14 20:58:04 -0400

    Update card_tienda_precio_product.blade.php

--- resources/views/components/card_tienda_precio_product.blade.php
+++ resources/views/components/card_tienda_precio_product.blade.php
@@ -56,7 +56,7 @@
                         <svg class="inline h-5 w-5" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg"><path d="M10 15.27L16.18 19L14.54 11.97L20 7.24L12.81 6.63L10 0L7.19 6.63L0 7.24L5.46 11.97L3.82 19L10 15.27Z" fill="#34BA00"/></svg>
                         <svg class="inline h-5 w-5" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg"><path d="M20 7.24L12.81 6.62L10 0L7.19 6.63L0 7.24L5.46 11.97L3.82 19L10 15.27L16.18 19L14.55 11.97L20 7.24ZM10 13.4V4.1L11.71 8.14L16.09 8.52L12.77 11.4L13.77 15.68L10 13.4Z" fill="#34BA00"/></svg>
                         <svg class="inline h-5 w-5" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg"><path d="M20 7.24L12.81 6.62L10 0L7.19 6.63L0 7.24L5.46 11.97L3.82 19L10 15.27L16.18 19L14.55 11.97L20 7.24ZM10 13.4L6.24 15.67L7.24 11.39L3.92 8.51L8.3 8.13L10 4.1L11.71 8.14L16.09 8.52L12.77 11.4L13.77 15.68L10 13.4Z" fill="#34BA00"/></svg>
-                        <span class="text-xs self-center mr-2">3.2</span>
+                        <span class="text-sm self-center mr-2">12</span>
                     </div>
                 </div>
                 <div class="col-span-2 self-center text-center hidden sm:block">
