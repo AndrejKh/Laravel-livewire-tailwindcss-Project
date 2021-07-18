ef6c8c7425b7ea8f5bd146f982c7e2ecb04b20f0
SilverAstarot <silverastarot@gmail.com>
2021-07-18 17:42:12 -0400

    Update tienda.blade.php

--- resources/views/tienda/tienda.blade.php
+++ resources/views/tienda/tienda.blade.php
@@ -57,9 +57,9 @@
 
         <div class="flex justify-center mt-3">
             <div class="max-w-7xl w-full">
-                <h4 class="font-bold text-xl text-gray-900 px-2 md:px-0">
+                <h2 class="font-bold text-xl text-gray-900 px-2 md:px-0">
                     ({{count($items)}}) Productos Disponibles
-                </h4>
+                </h2>
             </div>
         </div>
 
