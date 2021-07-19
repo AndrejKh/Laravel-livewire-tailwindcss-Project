864a5357f1d93bc7e398abdba83b8621a26b49aa
SilverAstarot <silverastarot@gmail.com>
2021-07-19 19:10:40 -0400

    miscelaneos

--- resources/views/tienda/product_detail.blade.php
+++ resources/views/tienda/product_detail.blade.php
@@ -74,7 +74,7 @@
                     </a>
                 </div>
                 <div class="max-w-7xl col-span-1 md:col-span-3 rounded-xl shadow-md overflow-hidden relative h-64 md:h-auto mx-2 md:mx-4" style="background-color:#fff!important;">
-                    <img class="h-full mx-auto" src="/storage/{{ $product->photo_main_product }}" alt="{{ $product->title }}" id="srcImageProduct" loading="lazy">
+                    <img class="mx-auto h-full md:h-52" src="/storage/{{ $product->photo_main_product }}" alt="{{ $product->title }}" id="srcImageProduct" loading="lazy">
                     <span class="absolute top-7 right-7 z-20 cursor-pointer" id="openShareModal">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
                     </span>
